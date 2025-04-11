58740879e9b47c9cbcab0fbd842f204e48d6e4b0
IT-Administrators <91905626+IT-Administrators@users.noreply.github.com>
2025-04-11 12:55:38 +0200

    Added testcases
    Added testcases for the Save() and Load() functions.

--- kvstore_test.go
+++ kvstore_test.go
@@ -4,15 +4,18 @@ import (
 	"testing"
 )
 
-var testKey = "T1"
-var testValue = "Test1"
+var testKey1 = "T1"
+var testValue1 = "Test1"
+var testKey2 = "T2"
+var testValue2 = "Test2"
+var saveFile = "./examples/store.gob"
 var kvs = NewKVStore[string, any]()
 
 func Test_Put(t *testing.T) {
 
-	kvs.Put(testKey, testValue)
-	kvs.Put("T2", "Test2")
-	ok := kvs.Data[testKey]
+	kvs.Put(testKey1, testValue1)
+	kvs.Put(testKey2, testValue2)
+	ok := kvs.Data[testKey1]
 	if ok == nil {
 		t.Errorf("could not insert value into store.")
 	}
@@ -20,23 +23,40 @@ func Test_Put(t *testing.T) {
 
 func Test_Get(t *testing.T) {
 
-	if ok := kvs.Data[testKey]; ok == nil {
-		t.Errorf("key %v does not exist", testKey)
+	if ok := kvs.Data[testKey1]; ok == nil {
+		t.Errorf("key %v does not exist", testKey1)
 	}
 }
 
 func Test_Update(t *testing.T) {
 
 	val := "This value was changed."
-	kvs.Update(testKey, val)
-	if val != kvs.Data[testKey] {
-		t.Errorf("got %v wanted %v", kvs.Data[testKey], val)
+	kvs.Update(testKey1, val)
+	if val != kvs.Data[testKey1] {
+		t.Errorf("got %v wanted %v", kvs.Data[testKey1], val)
 	}
 }
 
 func Test_Delete(t *testing.T) {
-	kvs.Delete(testKey)
-	if ok := kvs.Data[testKey]; ok != nil {
-		t.Errorf("could not remove %v from store.", testKey)
+	kvs.Delete(testKey1)
+	if ok := kvs.Data[testKey1]; ok != nil {
+		t.Errorf("could not remove %v from store.", testKey1)
+	}
+}
+
+func Test_SaveAndLoad(t *testing.T) {
+	kvs.Save(saveFile)
+	// Change key to test load.
+	kvs.Update(testKey2, "This key was changed")
+	kvs.Load(saveFile)
+	if val, _ := kvs.Get("T2"); val != "Test2" {
+		t.Errorf("got: %v expected: %v", val, testValue2)
+	}
+}
+
+func Test_Clear(t *testing.T) {
+	kvs.Clear()
+	if ok := kvs.Data[testKey1]; ok != nil {
+		t.Errorf("removal not succesffull; value: %v", kvs.Data[testKey1])
 	}
 }
